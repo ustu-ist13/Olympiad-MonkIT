308fc22e567294566adb40058777018c8b2ac502
xMonkIT <omelin.vladimir@gmail.com>
2016-10-05 17:47:16 +0300

    TaskB

---
+++ TaskB/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var place = Console.ReadLine().ToLower();
+            var row = int.Parse(place.Substring(0, place.Length - 1));
+            var position = place.Last();
+
+            const string window = "window";
+            const string aisle = "aisle";
+            const string neither = "neither";
+
+            string result;
+
+            switch (row)
+            {
+                case 1 - 2:
+                    // a bc d
+                    result = new[] { 'a', 'd' }.Contains(position) ? window : aisle;
+                    break;
+                case 3 - 20:
+                    // ab cd ef
+                    result = new[] { 'a', 'f' }.Contains(position) ? window : aisle;
+                    break;
+                default:
+                    // abc defg hjk
+                    result = new[] {'a', 'k'}.Contains(position)
+                        ? window
+                        : new[] {'c', 'd', 'g', 'h'}.Contains(position) ? aisle : neither;
+                    break;
+
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}

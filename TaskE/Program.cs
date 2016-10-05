5e86b1e0c42a0a04ce34fede590d251e38bd5db3
xMonkIT <omelin.vladimir@gmail.com>
2016-10-05 18:27:01 +0300

    TaskE

---
+++ TaskE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = Console.ReadLine().Split().Select(int.Parse).ToList()[1];
+            var bumbums = 0;
+            var droids = 0;
+
+            Console.ReadLine().Split().Select(int.Parse).ToList().ForEach(x =>
+            {
+                if (x > k) bumbums += x - k;
+                else droids += k - x;
+            });
+
+            Console.WriteLine($"{bumbums} {droids}");
+        }
+    }
+}

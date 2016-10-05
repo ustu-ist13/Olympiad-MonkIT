2aaa54dd49360f16d13849aaf38ad44ad3926201
xMonkIT <omelin.vladimir@gmail.com>
2016-10-05 18:11:23 +0300

    TaskC

---
+++ TaskC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+
+            var sections = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var sums = Enumerable.Range(0, sections.Count - 2).Select(x => sections.GetRange(x, 3).Sum());
+            var max = sums.Max();
+
+            Console.WriteLine($"{max} {sums.ToList().IndexOf(max) + 2}");
+        }
+    }
+}

b0249e2a0568b04649f652a5dc7fe85d46ae44a8
xMonkIT <omelin.vladimir@gmail.com>
2016-10-05 18:33:24 +0300

    add v2 to TaskE

--- TaskE/Program.cs
+++ TaskE/Program.cs
@@ -8,16 +8,23 @@ namespace TaskE
         static void Main(string[] args)
         {
             var k = Console.ReadLine().Split().Select(int.Parse).ToList()[1];
-            var bumbums = 0;
-            var droids = 0;
 
-            Console.ReadLine().Split().Select(int.Parse).ToList().ForEach(x =>
-            {
-                if (x > k) bumbums += x - k;
-                else droids += k - x;
-            });
+            // v1
+            //var bumbums = 0;
+            //var droids = 0;
 
-            Console.WriteLine($"{bumbums} {droids}");
+            //Console.ReadLine().Split().Select(int.Parse).ToList().ForEach(x =>
+            //{
+            //    if (x > k) bumbums += x - k;
+            //    else droids += k - x;
+            //});
+
+            //Console.WriteLine($"{bumbums} {droids}");
+
+            // v2
+            var bumbums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Console.WriteLine($"{bumbums.Where(x => x > k).Select(x => x - k).Sum()} {bumbums.Where(x => x < k).Select(x => k - x).Sum()}");
         }
     }
 }
